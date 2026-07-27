1aa37aee86fbf32ea2cb6b3bda29ac194a85d8b9
Renisson Rodrigues de Souza <renisson.souza@docente.senai.br>
2026-07-27 19:47:31 -0300

    27-07-20266

--- Main.py
+++ Main.py
@@ -1,4 +1,14 @@
+import csv
+from datetime import datetime
+
 saldo = 100
+extrato = []
+ARQUIVO_CSV = "extrato_movimentacao.csv"
+
+def salvar_no_csv(tipo, valor):
+    with open(ARQUIVO_CSV, mode="a", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerow([datetime.now().strftime("%d/%m/%Y %H:%M:%S"), tipo, f"{valor:.2f}", f"{saldo:.2f}"])
 
 def exibir_banco():
     print("\n===== CAIXA ELETRONICO =====")
@@ -10,24 +20,41 @@ def exibir_banco():
 
 def consultar_saldo():
     print(f"\nSeu saldo atual é: R$ {saldo}")
+    pass
 
 def depositar_dinheiro():
     global saldo
     valor = float(input("\nDigite o valor a ser depositado R$ "))
     if valor > 0:
         saldo += valor
+        extrato.append(f"Depósito: R$ {valor:.2f}")
+        salvar_no_csv("Depósito", valor)
         print(f"\nDeposito de R$ {valor} realizado com sucesso.")
     else:
         print("\nValor inválido.")
+    pass
 
 def sacar_dinheiro():
     global saldo
     valor = float(input("\nDigite o valor a ser sacado R$ "))
     if valor > 0 and valor <= saldo:
         saldo -= valor
+        extrato.append(f"Saque: R$ {valor:.2f}")
+        salvar_no_csv("Saque", valor)
         print(f"\nSaque de R$ {valor} realizado com sucesso.")
     else:
         print("\nSaldo insuficiente ou valor inválido.")
+    pass
+
+def ver_extrato():
+    print("\n===== EXTRATO =====")
+    if not extrato:
+        print("Nenhuma movimentação realizada.")
+    else:
+        for movimentacao in extrato:
+            print(movimentacao)
+    print(f"\nSaldo atual: R$ {saldo:.2f}")
+    pass
 
 def main():
     while True:
@@ -40,7 +67,7 @@ def main():
         elif opcao == "3":
             sacar_dinheiro()
         elif opcao == "4":
-            print("\nExtrato não disponível no momento.")
+            ver_extrato()
         elif opcao == "5":
             print("\nSaindo do sistema. Obrigado por utilizar o Caixa Eletrônico.")
             break
